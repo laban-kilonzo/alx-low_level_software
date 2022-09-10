c824ec893f8158d1891edbad6b28c17e03447ddf
laban-kilonzo <labanmutisya7@gmail.com>
2022-09-10 08:40:58 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
+ * main - print the string in the put function
  *
- * Return:Always 0
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
